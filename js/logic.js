2fc70ee116143a7c6a85db3b89a97c9cae0804a7
Robert Petermeier <robert.petermeier@gmx.de>
2012-12-27 14:01:42 +0100

    Data is loaded initially from local storage

--- js/logic.js
+++ js/logic.js
@@ -1,9 +1,13 @@
 ﻿var vm;
 
-function Hanzi(pinyin, hanzi) {
+function Hanzi(pinyin, hanzi, timestamp) {
 	this.pinyin = pinyin;
 	this.hanzi = hanzi;
-	this.timestamp = new Date();
+	if (timestamp == null) {
+		this.timestamp = new Date();
+	} else {
+		this.timestamp = timestamp;
+	}
 }
 
 Hanzi.prototype.representation = function() {
@@ -11,24 +15,32 @@ Hanzi.prototype.representation = function() {
 }
 
 function initializeHanzi() {
+	var data;
+	// alert(JSON.stringify(data));
 	// Retrieve static data
-	var data = [
+	/*data = [
 		new Hanzi("nǐ", "你"),
 		new Hanzi("hǎo", "好"),
 		new Hanzi("ma", "吗"),
 		new Hanzi("tiān", "天"),
 		new Hanzi("qì", "气")
-	];
+	];*/
 	
 	// Retrieve from JSON file
-	// alert(JSON.stringify(data));
 	// $.getJSON('json/hanzi.json', function(jsonData) {
 	//	
 	// });
 	
 	// Retrieve from local storage
-	var storedData = $.jStorage.get("list-of-hanzi");
-	
+	// var storedData = $.jStorage.get("list-of-hanzi");
+	// data = storedData;
+	// $.jStorage.set("list-of-hanzi", data);
+	var localdata = $.jStorage.get("list-of-hanzi");
+	data = [];
+	for (var ii = 0; ii < localdata.length; ++ii) {
+		data[ii] = new Hanzi(localdata[ii].pinyin, localdata[ii].hanzi, localdata[ii].timestamp);
+	}
+
 	return data;
 }
 
@@ -38,13 +50,13 @@ var HanziViewModel = function() {
 
 	this.currentData = ko.observableArray(initializeHanzi());
 	this.generatedData = ko.computed(function() {
-		// It is important to call the function data() here instead of accessing the property data
+		// It is important to call the function currentData() here instead of accessing the property data
 		return this.currentData();
 	}, this);
 	
 	this.generateNewData = function() {
 		// this.currentData().removeAll();
-		// It is important to access the propery this.currentData here, not the function this.currentData()
+		// It is important to access the property this.currentData here, not the function this.currentData()
 		this.currentData.push(new Hanzi("tiān", "天"));
 	};
 	
