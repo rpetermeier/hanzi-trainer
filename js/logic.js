801918fbb7deef0448ee7cd6a450236f3d502879
Robert Petermeier <robert.petermeier@gmx.de>
2013-05-06 21:47:21 +0200

    One more hanzi
    (cherry picked from commit 79eb8763a4550d83f2bdbd677c8a68c2affa64f7)

--- js/logic.js
+++ js/logic.js
@@ -215,7 +215,7 @@ var HanziViewModel = function() {
 	};
 	
 	this.loadJsonDataFromServer = function() {
-		$.getJSON('json/hanzi/2013-04-28.json', function(jsonData) {
+		$.getJSON('json/hanzi/2013-05-06.json', function(jsonData) {
 			vm.rebuildFromServerJson(jsonData);
 			vm.alert("Laden war erfolgreich", "Das Laden der Hanzi vom Server war erfolgreich.<br/>Das Vokabular umfasst jetzt " + vm.totalNumberOfHanzi() + " Hanzi.");
 		});
